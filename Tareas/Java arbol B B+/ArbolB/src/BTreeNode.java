47636c2cc9378dac08620dd4f41d4bdccf28db95
Juandavidsabo12 <juandavidsabo12@gmail.com>
2026-08-16 18:35:17 -0500

    BTree Second update (serach,insertion and splitChild classes)

--- Tareas/Java arbol B B+/ArbolB/src/BTreeNode.java
+++ Tareas/Java arbol B B+/ArbolB/src/BTreeNode.java
@@ -54,8 +54,30 @@ public class BTreeNode {
         }
     }
 
-    public void splitChild(int i,BTreeNode node){
+    public void splitChild(int i, BTreeNode y) {
+        BTreeNode z = new BTreeNode(y.t, y.leaf);
+        z.n = t - 1;
 
+        for (int j = 0; j < t - 1; j++)
+            z.keys[j] = y.keys[j + t];
+
+        if (!y.leaf) {
+            for (int j = 0; j < t; j++)
+                z.children[j] = y.children[j + t];
+        }
+
+        y.n = t - 1;
+
+        for (int j = n; j >= i + 1; j--)
+            children[j + 1] = children[j];
+
+        children[i + 1] = z;
+
+        for (int j = n - 1; j >= i; j--)
+            keys[j + 1] = keys[j];
+
+        keys[i] = y.keys[t - 1];
+        n++;
     }
 
 }
